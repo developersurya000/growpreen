56e516c748bb48c7cc1a54ad2a5facb7702ffccd
surya <suryanagendra061@gmail.com>
2026-02-04 22:01:03 +0530

    Update admin referral logic

--- growpreen-backend/server.js
+++ growpreen-backend/server.js
@@ -21,7 +21,7 @@ const app = express();
 
 const allowedOrigins = [
   'http://localhost:5173',
-'https://super-cannoli-1648a9.netlify.app', // replace after Netlify URL
+'https://growpreen.netlify.app/', // replace after Netlify URL
 ];
 
 app.use(cors({
